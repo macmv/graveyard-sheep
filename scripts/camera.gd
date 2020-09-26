0d0decdd243f89d955f056d447013adf196471fb
Neil Macneale <neil.macneale.v@gmail.com>
2020-09-25 17:51:15 -0700

    Implemented camera shake

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,7 +1,16 @@
 extends Camera2D
 
+export(float) var shake_start = 20
+export(float) var shake_decrease = 2
+var shake_amount = 0
+
 func on_hurt():
-  set_offset(Vector2( \
-    rand_range(-1.0, 1.0) * 20, \
-    rand_range(-1.0, 1.0) * 20 \
-  ))
+  shake_amount = shake_start
+
+func _process(delta):
+  if shake_amount > 0:
+    set_offset(Vector2( \
+      rand_range(-1.0, 1.0) * shake_amount, \
+      rand_range(-1.0, 1.0) * shake_amount \
+    ))
+    shake_amount -= shake_decrease
