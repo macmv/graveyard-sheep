963081a4873a81e19f1f5798fb044e09f4567c1a
Neil Macneale <neil.macneale.v@gmail.com>
2020-12-21 21:25:24 -0800

    Updated attach animation to be a dash forward animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,6 @@ export(float) var speed            = 120
 export(float) var friction         = 0.8
 export(float) var knockback_amount = 2
 
-export(NodePath) var sprite_path
-export(NodePath) var collision_ray_path
-export(NodePath) var attack_animation_path
-onready var sprite           = get_node(sprite_path)
-onready var collision_ray    = get_node(collision_ray_path)
-onready var attack_animation = get_node(attack_animation_path)
 var wool
 var camera
 var vel = Vector2(0, 0)
@@ -19,19 +13,19 @@ func _physics_process(delta):
   var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
   var move_vec = Vector2(x, y).normalized()
   if move_vec.length_squared() > 1e-05:
-    sprite.play()
+    $Sprite.play()
     rotation = -move_vec.angle_to(Vector2(1, 0))
-    vel += move_vec
+    vel += move_vec * speed
   else:
-    sprite.stop()
+    $Sprite.stop()
   vel *= friction
-  move_and_slide(vel * speed)
+  move_and_slide(vel)
 
   if Input.is_action_just_pressed("attack"):
-    attack_animation.show()
-    attack_animation.play()
-    attack_animation.frame = 0
-    for body in collision_ray.get_overlapping_bodies():
+    $DashAnimation.show()
+    $DashAnimation.play()
+    $DashAnimation.frame = 0
+    for body in $HitTrigger.get_overlapping_bodies():
       body.damage(position, 1)
       wool.add(1)
 
