2125500484456a3d1cdaa23c7c419f98370432d0
Neil Macneale <neil.macneale.v@gmail.com>
2021-01-09 17:16:48 -0800

    Enemies are knocked back and stunned on hit

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export(float) var speed            = 30
 export(float) var friction         = 0.85
-export(float) var knockback_amount = 10
+export(float) var knockback_amount = 2000
 export(float) var attack_range     = 140
 export(float) var view_distance    = 1000
 
@@ -14,15 +14,21 @@ var vel = Vector2(0, 0)
 var health = 5
 var player
 var iframes = 0
+var stun = 0
 
 func _ready():
   player = get_parent().get_node(get_parent().player_path)
   $Health.text = str(health)
 
 func _physics_process(delta):
-  var move_vec = (player.position - position).normalized()
-  rotation = -move_vec.angle_to(Vector2(0, -1))
-  vel += move_vec * speed
+  if stun > 0:
+    stun -= delta
+  if stun < 0:
+    stun = 0
+  if stun == 0:
+    var move_vec = (player.position - position).normalized()
+    rotation = -move_vec.angle_to(Vector2(0, -1))
+    vel += move_vec * speed
   vel *= friction
   move_and_slide(vel)
 
@@ -42,9 +48,10 @@ func damage(from_pos, amount) -> bool:
     return false
   else:
     iframes = 0.3
+    stun = 0.25
     health -= amount
     $Health.text = str(health)
-    vel += (position - from_pos).normalized() * amount * knockback_amount
+    vel = (position - from_pos).normalized() * knockback_amount
     if health <= 0:
       get_parent().remove_child(self)
     return true
