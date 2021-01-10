7284203ae55c3f46a1307800acec34ca4b616f0f
Neil Macneale <neil.macneale.v@gmail.com>
2021-01-09 16:37:09 -0800

    Added new sheep animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,8 +17,7 @@ func _physics_process(delta):
   if dash_time > 0:
     dash_time -= delta
     if dash_time < 0:
-      $DashAnimation.playing = false
-      $DashAnimation.hide()
+      $Sprite.animation = "run"
       dash_time = 0
     vel += dash_vec * dash_speed
   else:
@@ -26,19 +25,16 @@ func _physics_process(delta):
     var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
     move_vec = Vector2(x, y).normalized()
     if move_vec.length_squared() > 1e-05:
-      $Sprite.play()
+      $Sprite.animation = "run"
       rotation = -move_vec.angle_to(Vector2(1, 0))
       vel += move_vec * speed
     else:
-      $Sprite.stop()
+      $Sprite.animation = "idle"
   vel *= friction
   move_and_slide(vel)
 
   if dash_time == 0 && Input.is_action_just_pressed("attack") && move_vec.length_squared() > 1e-05:
-    $DashAnimation.show()
-    $DashAnimation.playing = true
-    $DashAnimation.frame = 0
-    $Sprite.play()
+    $Sprite.animation = "dash"
     dash_time = dash_length
     dash_vec = move_vec
 
